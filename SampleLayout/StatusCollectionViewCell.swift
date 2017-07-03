9825715bab9b2f12ab982b5790e924370f19d1e0
radhakrishnan S <krish.selvaraj@gmail.com>
2017-07-03 21:23:03 +0530

    Integrated the basic flow

---
+++ SampleLayout/StatusCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  StatusCollectionViewCell.swift
+//  SampleLayout
+//
+//  Created by radhakrishnan S on 03/07/17.
+//  Copyright © 2017 Test. All rights reserved.
+//
+
+import UIKit
+
+class StatusCollectionViewCell: UICollectionViewCell {
+    
+}
