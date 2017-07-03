71c1cd3d93dc4e89e9e29aeff66248dfef6d1976
radhakrishnan S <krish.selvaraj@gmail.com>
2017-07-03 13:40:46 +0530

    Second region development in progress

---
+++ SampleLayout/BannerCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  BannerCollectionViewCell.swift
+//  SampleLayout
+//
+//  Created by radhakrishnan S on 03/07/17.
+//  Copyright © 2017 Test. All rights reserved.
+//
+
+import UIKit
+
+class BannerCollectionViewCell: UICollectionViewCell {
+    
+}
